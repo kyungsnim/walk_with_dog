07712bbc90a8c426f3b892d6c39cf9cdc6d3a2a4
hwang <skyboom86@gmail.com>
2021-12-22 00:14:22 +0900

    ### 2021.12.22(수) - 산책화면 구현 중 (polyLine 구현 필요)

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,8 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:get/get.dart';
 import 'package:walk_with_dog/screens/my_screen.dart';
-
-import '../main.dart';
+import 'package:walk_with_dog/screens/walk2_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   final int getPageIndex;
@@ -34,7 +32,7 @@ class _HomeScreenState extends State<HomeScreen> with WidgetsBindingObserver {
     });
     pageController = PageController(
       // 다른 페이지에서 넘어올 때도 controller를 통해 어떤 페이지 보여줄 것인지 셋팅
-        initialPage: getPageIndex != null ? this.getPageIndex : 0);
+        initialPage: 0);
 
     // WidgetsBinding.instance!.addObserver(this);
     // setStatus('Online');
@@ -48,13 +46,13 @@ class _HomeScreenState extends State<HomeScreen> with WidgetsBindingObserver {
 
   whenPageChanges(int pageIndex) {
     setState(() {
-      this.getPageIndex = pageIndex;
+      getPageIndex = pageIndex;
     });
   }
 
   onTapChangePage(int pageIndex) {
     pageController!
-        .animateToPage(pageIndex, duration: Duration(milliseconds: 100), curve: Curves.bounceInOut);
+        .animateToPage(pageIndex, duration: const Duration(milliseconds: 100), curve: Curves.bounceInOut);
   }
 
   @override
@@ -67,73 +65,76 @@ class _HomeScreenState extends State<HomeScreen> with WidgetsBindingObserver {
         child: Scaffold(
           body: PageView(
             children: [
+              const Walk2Screen(),
               Container(),
               Container(),
-              Container(),
-              MyScreen(),
+              const MyScreen(),
             ],
             controller: pageController, // controller를 지정해주면 각 페이지별 인덱스로 컨트롤 가능
             onPageChanged:
             whenPageChanges, // page가 바뀔때마다 whenPageChanges 함수가 호출되고 현재 pageIndex 업데이트해줌
           ),
-          bottomNavigationBar: BottomNavigationBar(
-            // Bar에 텍스트 라벨 안보이게 변경
-            // showSelectedLabels: false,
-            // showUnselectedLabels: false,
-            backgroundColor: Colors.white,
-            currentIndex: this.getPageIndex,
-            onTap: onTapChangePage,
-            selectedItemColor: Colors.black,
-            selectedIconTheme: IconThemeData(size: 40),
-            selectedFontSize: 16,
-            selectedLabelStyle:
-            TextStyle(fontFamily: 'Nanum', fontWeight: FontWeight.bold, color: Colors.black),
-            unselectedItemColor: Colors.grey,
-            unselectedFontSize: 12,
-            // iconSize: 20,
-            type: BottomNavigationBarType.fixed,
-            items: <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                  icon: Image.asset(
-                    'assets/icon/bottom1.png',
-                    width: 20,
-                  ),
-                  activeIcon: Image.asset(
-                    'assets/icon/bottom1.png',
-                    width: 30,
-                  ),
-                  label: '산책'),
-              BottomNavigationBarItem(
-                  icon: Image.asset(
-                    'assets/icon/bottom2.png',
-                    width: 15,
-                  ),
-                  activeIcon: Image.asset(
-                    'assets/icon/bottom2.png',
-                    width: 25,
-                  ),
-                  label: '기록'),
-              BottomNavigationBarItem(
-                  icon: Image.asset(
-                    'assets/icon/bottom3.png',
-                    width: 20,
-                  ),
-                  activeIcon: Image.asset(
-                    'assets/icon/bottom3.png',
-                    width: 30,
-                  ),
-                  label: '플레이스'),
-              BottomNavigationBarItem(
-                  icon: Image.asset(
-                    'assets/icon/bottom4.png',
-                    width: 20,
-                  ),
-                  activeIcon: Image.asset(
-                    'assets/icon/bottom4.png',
-                    width: 30,
-                  ),
-                  label: 'MY'),
-            ],
+          bottomNavigationBar: SizedBox(
+            height: Get.height * 0.15,
+            child: BottomNavigationBar(
+              // Bar에 텍스트 라벨 안보이게 변경
+              // showSelectedLabels: false,
+              // showUnselectedLabels: false,
+              backgroundColor: Colors.white,
+              currentIndex: getPageIndex,
+              onTap: onTapChangePage,
+              selectedItemColor: Colors.black,
+              selectedIconTheme: const IconThemeData(size: 40),
+              selectedFontSize: 16,
+              selectedLabelStyle:
+              const TextStyle(fontFamily: 'Nanum', fontWeight: FontWeight.bold, color: Colors.black),
+              unselectedItemColor: Colors.grey,
+              unselectedFontSize: 12,
+              // iconSize: 20,
+              type: BottomNavigationBarType.fixed,
+              items: <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                    icon: Image.asset(
+                      'assets/icon/bottom1.png',
+                      width: 20,
+                    ),
+                    activeIcon: Image.asset(
+                      'assets/icon/bottom1.png',
+                      width: 30,
+                    ),
+                    label: '산책'),
+                BottomNavigationBarItem(
+                    icon: Image.asset(
+                      'assets/icon/bottom2.png',
+                      width: 15,
+                    ),
+                    activeIcon: Image.asset(
+                      'assets/icon/bottom2.png',
+                      width: 25,
+                    ),
+                    label: '기록'),
+                BottomNavigationBarItem(
+                    icon: Image.asset(
+                      'assets/icon/bottom3.png',
+                      width: 20,
+                    ),
+                    activeIcon: Image.asset(
+                      'assets/icon/bottom3.png',
+                      width: 30,
+                    ),
+                    label: '플레이스'),
+                BottomNavigationBarItem(
+                    icon: Image.asset(
+                      'assets/icon/bottom4.png',
+                      width: 20,
+                    ),
+                    activeIcon: Image.asset(
+                      'assets/icon/bottom4.png',
+                      width: 30,
+                    ),
+                    label: 'MY'),
+              ],
+            ),
           ),
         ),
       ),
@@ -145,11 +146,11 @@ class _HomeScreenState extends State<HomeScreen> with WidgetsBindingObserver {
         context: context,
         builder: (BuildContext context) {
           return AlertDialog(
-            title: Text('앱 종료',
+            title: const Text('앱 종료',
                 style: TextStyle(
                   fontFamily: 'Binggrae',
                 )),
-            content: Text(
+            content: const Text(
               '종료하시겠습니까?',
               style: TextStyle(fontFamily: 'Binggrae'),
             ),
@@ -157,7 +158,7 @@ class _HomeScreenState extends State<HomeScreen> with WidgetsBindingObserver {
               ElevatedButton(
                 child: Container(
                   padding: const EdgeInsets.all(8),
-                  child: Text('확인', style: TextStyle(fontFamily: 'Binggrae', fontSize: 16)),
+                  child: const Text('확인', style: TextStyle(fontFamily: 'Binggrae', fontSize: 16)),
                 ),
                 style: ElevatedButton.styleFrom(primary: Colors.blueAccent),
                 onPressed: () async {
@@ -167,7 +168,7 @@ class _HomeScreenState extends State<HomeScreen> with WidgetsBindingObserver {
               ElevatedButton(
                 child: Container(
                   padding: const EdgeInsets.all(8),
-                  child: Text('취소', style: TextStyle(fontFamily: 'Binggrae', fontSize: 16)),
+                  child: const Text('취소', style: TextStyle(fontFamily: 'Binggrae', fontSize: 16)),
                 ),
                 style: ElevatedButton.styleFrom(primary: Colors.grey),
                 onPressed: () async {
